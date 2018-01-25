300e3019977e5969f0d0f439751cc8b7d77904b0
wesdriver45 <wesdriver45@gmail.com>
2018-01-24 20:25:48 -0500

    server and controllers

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var port = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//handlebars setup
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+//route to server
+var routes = require("./controllers/brewControllers.js");
+
+app.use(routes);
+
+app.listen(PORT, function()) {
+	console.log("App now listening at localhost: " + PORT);
+
+});
