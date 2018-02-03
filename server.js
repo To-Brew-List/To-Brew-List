c1ec4c23363921ab1f216534128f182ed7a1e388
Connor Leach <leachc820@gmail.com>
2018-02-02 23:39:56 -0500

    finishing touches

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require("path");
 var materialize = require("materialize-css");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3306;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
